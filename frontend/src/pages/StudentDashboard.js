20eae755a2ea9a7b425a848ad77cd947860232b8
jayantgoyal1502 <jayantgoyal500@gmail.com>
2025-05-11 15:28:59 +0530

    Expired jobs removed from student dashboard

--- frontend/src/pages/StudentDashboard.js
+++ frontend/src/pages/StudentDashboard.js
@@ -26,7 +26,9 @@ const StudentDashboard = () => {
             const { data } = await customApi.get("/jobs", {
                 headers: { Authorization: `Bearer ${token}` },
             });
-            setJobs(data);
+            const currentDate = new Date();
+            const filteredJobs = data.filter(job => new Date(job.application_deadline) > currentDate);
+            setJobs(filteredJobs);
         } catch (error) {
             console.error("Error fetching jobs", error);
         }
